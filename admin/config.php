f3943e5267c6f3c3f4b2d4939fd4fb02916aa4e1
Sergio Serrano <sergiofst@gmail.com>
2013-02-23 13:03:01 -0600

    3 lilas

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/oc_3lilas/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/oc_3lilas/');
+define('HTTP_IMAGE', 'http://127.0.0.1/oc_3lilas/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/oc_3lilas/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/oc_3lilas/');
+define('HTTPS_IMAGE', 'http://127.0.0.1/oc_3lilas/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/admin/');
+define('DIR_SYSTEM', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/');
+define('DIR_DATABASE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/database/');
+define('DIR_LANGUAGE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/admin/language/');
+define('DIR_TEMPLATE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/admin/view/template/');
+define('DIR_CONFIG', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/config/');
+define('DIR_IMAGE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/image/');
+define('DIR_CACHE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/download/');
+define('DIR_LOGS', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/logs/');
+define('DIR_CATALOG', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc_3lilas');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/oc_3lilas/');
+define('HTTP_IMAGE', 'http://127.0.0.1/oc_3lilas/image/');
+define('HTTP_ADMIN', 'http://127.0.0.1/oc_3lilas/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/oc_3lilas/');
+define('HTTPS_IMAGE', 'http://127.0.0.1/oc_3lilas/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/catalog/');
+define('DIR_SYSTEM', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/');
+define('DIR_DATABASE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/database/');
+define('DIR_LANGUAGE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/catalog/language/');
+define('DIR_TEMPLATE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/config/');
+define('DIR_IMAGE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/image/');
+define('DIR_CACHE', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/download/');
+define('DIR_LOGS', 'C:\Aplicaciones\xampp\htdocs\oc_3lilas/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc_3lilas');
+define('DB_PREFIX', '');
+?>
